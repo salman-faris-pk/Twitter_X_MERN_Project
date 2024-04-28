f5d4ce9bdbf316b7951ff30d206c762d4b292ceb
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-04-28 16:50:14 +0530

    project settup completed

---
+++ backend/db/mongodbConnects.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectMongoDb= async()=>{
+    try{
+     const connect= await mongoose.connect(process.env.MONGO_URL)
+     console.log(`MongoDB connected: ${connect.connection.host}`);
+
+    }catch(err){
+      console.log(`Error Connection to Mongodb: ${err.message}`);
+      process.exit(1);
+    }
+}
+
+export default connectMongoDb 
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+const app=express();
+import dotenv from "dotenv"
+dotenv.config();
+ const port=process.env.PORT
+ import connectMongoDb from "./db/mongodbConnects.js"
+ import authRoutes from "./routes/auth.routes.js"
+
+
+app.use('/api/auth',authRoutes)
+
+
+
+
+app.listen(port,()=>{
+    console.log("server running on ",port);
+    connectMongoDb();
+})
+
