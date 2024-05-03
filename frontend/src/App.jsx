6d50bea2e6a8b6d494f50bdb0fcf58d308459113
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-05-03 18:12:02 +0530

    designing ui of Home and other components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,29 @@ import { Routes,Route } from "react-router-dom"
 import Homepage from "./pages/home/Homepage"
 import SignUpPage from "./pages/auth/signup/SignUpPage"
 import Loginpage from "./pages/auth/login/Loginpage"
+import Sidebar from "./components/common/Sidebar"
+import RightPanel from "./components/common/RightPanel"
 
 
 function App() {
   
 
   return (
-    
+
     <div className='flex max-w-6xl mx-auto'>
+         
+         <Sidebar/>
+
         <Routes>
 
         <Route path='/' element={<Homepage />} />
 				<Route path='/signup' element={<SignUpPage />} />
 				<Route path='/login' element={<Loginpage />} />
           
-          </Routes>     
+        </Routes> 
+
+        <RightPanel/>
+
     </div>
   )
 }
