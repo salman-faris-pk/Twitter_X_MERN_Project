26e08a79b5753f2e8e977e01fc32b6fac8960d0c
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-05-03 19:27:07 +0530

    front end ui's completes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import SignUpPage from "./pages/auth/signup/SignUpPage"
 import Loginpage from "./pages/auth/login/Loginpage"
 import Sidebar from "./components/common/Sidebar"
 import RightPanel from "./components/common/RightPanel"
+import NotificationPage from "./pages/notification/NotificationPage"
+import ProfilePage from "./pages/profile/ProfilePage"
 
 
 function App() {
@@ -20,7 +22,9 @@ function App() {
         <Route path='/' element={<Homepage />} />
 				<Route path='/signup' element={<SignUpPage />} />
 				<Route path='/login' element={<Loginpage />} />
-          
+        <Route path='/notifications' element={<NotificationPage />}/>
+        <Route path='/profile/:username' element={<ProfilePage />} />
+
         </Routes> 
 
         <RightPanel/>
